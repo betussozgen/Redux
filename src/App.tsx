2bbaacd40129eddd6811a159d8dfd5441326c30c
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-15 13:43:18 +0300

    redux örnegi

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,71 @@ import React from 'react';
 import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 
 
+import { createStore } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
-function App() {
+//STORE
+//global olarak tutacağım state'leri taımlıyorum.
+const initialState = {
+  counter: 0,
+}
 
+
+//REDUCER
+function reducers(state, action) {
+  switch (action.type) {
+    case 'UPDATE_COUNTER':
+      return { ...state, counter: state.counter + 1 };
+    default:
+      return state;
+  }
+
+}
+
+export default () => {
   return (
-    <SafeAreaView>
-      <Text>Hello Redux</Text>
-    </SafeAreaView>
+    <Provider store={createStore(reducers, initialState)}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <First />
+        <Second />
+      </SafeAreaView>
+    </Provider>
   );
 }
 
 
-export default App;
+const First = () => {
+
+  const counter = useSelector(selector => selector.counter);
+
+  const dispatch = useDispatch();
+
+  const handleUpdate = () => {
+    dispatch({ type: 'UPDATE_COUNTER' })
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#eceff1' }}>
+      <Text style={{ fontSize: 30 }}>First:  {counter}</Text>
+      <Button title='UPDATE' onPress={handleUpdate} />
+    </View>
+  )
+}
+
+const Second = () => {
+
+  const counter = useSelector(selector => selector.counter);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Text style={{ fontSize: 30 }}>Second: {counter}</Text>
+    </View>
+  )
+}

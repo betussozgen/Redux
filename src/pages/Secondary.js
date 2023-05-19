024caaee2a6f4a627c91a2047701e9063f8e0947
betussozgen <fatmabetulozgen@hotmail.com>
2023-05-19 14:18:03 +0300

    Listelemeyi gerçekleştirdik.

--- src/pages/Secondary.js
+++ src/pages/Secondary.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
+import { useSelector } from 'react-redux';
 
 function Secondary() {
+    //global state'lerim için selektor kullanıyorum.
+    const list = useSelector(s => s.nameList)
     return (
         <View>
             <Text>Secondary</Text>
+            <FlatList
+                keyExtractor={(_, index) => index.toString()}
+                data={list}
+                renderItem={({ item }) => <Text>{item}</Text>} />
         </View>
     )
 
